bd07246a31889462658a163c23f88150672d60ae
AurelDev <38510448+AeRogue666@users.noreply.github.com>
2025-10-24 17:20:21 +0200

    Updating productPageContent.vue CSS classes and adding new Molecule component productCollapsibleContent and new i18n locale text, Updating locales with new locale text for productPageContent.vue page, Creating productCollapsibleContent.vue who replace productCollapsibleMarkdown.vue (have been deleted) with a v-if system to customize the content of the collapsible.

---
+++ components/product/molecules/productCollapsibleContent.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { capitalize } from 'vue';
+
+defineProps<{
+    title: string;
+    description?: any;
+    ingredients?: string;
+    allergies?: any;
+    nutrition?: any;
+    brand?: string;
+    origin?: string;
+    tags?: any;
+}>();
+</script>
+
+<template>
+    <UCollapsible class="flex flex-col items-baseline gap-2 mx-6 my-3">
+        <UButton class="group" color="neutral" variant="ghost" trailing-icon="fa6-solid:chevron-down" :ui="{
+            trailingIcon: 'group-data-[state=open]:rotate-180 transition-transform duration-200'
+        }" block>
+            <span class="text-base font-semibold">{{ capitalize(title) }}</span>
+        </UButton>
+        <template #content>
+            <ProductMoleculesProductDescriptionMarkdownMolecule v-if="description" :source="description" />
+            <div class="flex flex-col items-baseline w-full h-full gap-6">
+                <div v-if="ingredients" class="flex flex-col items-baseline">
+                    <span>{{ capitalize($t('main.product.collapsible.ingredient')) }}:</span>
+                    <ProductMoleculesProductDescriptionMarkdownMolecule :source="ingredients" />
+                </div>
+                <div v-if="allergies" class="flex flex-col items-baseline">
+                    <span>{{ capitalize($t('main.product.collapsible.allergy')) }}:</span>
+                    <div>{{ allergies }}</div>
+                </div>
+                <div v-if="nutrition" class="flex flex-col items-baseline">
+                    <span>{{ capitalize($t('main.product.collapsible.nutrition')) }}:</span>
+                    <div>{{ nutrition }}</div>
+                </div>
+                <span v-if="brand">{{ capitalize($t('main.product.collapsible.brand')) }}: {{ brand }}</span>
+                <span v-if="origin">{{ capitalize($t('main.product.collapsible.origin')) }}: {{ origin }}</span>
+                <div v-if="tags" class="flex flex-col items-baseline">
+                    <span>{{ capitalize($t('main.product.collapsible.tag')) }}:</span>
+                    <div>{{ tags }}</div>
+                </div>
+            </div>
+        </template>
+    </UCollapsible>
+</template>
--- components/product/organisms/productPageContent.vue
+++ components/product/organisms/productPageContent.vue
@@ -21,7 +21,7 @@ defineProps<{
             value: string,
             color: string,
         },
-        image?: {
+        image: {
             id: string,
             description: string,
             width?: number,
@@ -46,7 +46,7 @@ const emit = defineEmits<{
 
 <template>
     <article v-for="(product, i) of productsList"
-        class="relative group/blog-post rounded-lg overflow-hidden flex flex-col w-full h-screen mt-3 bg-default gap-2"
+        class="relative group/blog-post rounded-lg overflow-hidden flex flex-col w-full h-auto mt-3 bg-default gap-2"
         :key="i">
         <UContainer class="flex flex-col lg:flex-row">
             <div class="relative overflow-hidden w-full pointer-events-none">
@@ -59,7 +59,8 @@ const emit = defineEmits<{
             <UContainer class="flex flex-col items-baseline my-3">
                 <UContainer class="flex flex-col items-baseline">
                     <div class="flex items-baseline w-full">
-                        <h1 class="text-2xl text-pretty font-semibold text-highlighted">{{ capitalize(product.title) }}</h1>
+                        <h1 class="text-2xl text-pretty font-semibold text-highlighted">{{ capitalize(product.title) }}
+                        </h1>
                     </div>
                     <div class="flex flex-row justify-between items-center w-full my-3">
                         <div class="flex flex-row items-baseline w-auto gap-6">
@@ -72,16 +73,27 @@ const emit = defineEmits<{
                         <div class="flex flex-col items-baseline w-auto">
                             <span v-if="product.availability" class="flex flex-row items-center text-base h-7 gap-2">
                                 <UIcon name="i-fa6-solid:circle" class="size-5" :class="product.availability?.color" />
-                                {{ capitalize($t('main.product.availability.' + product.availability.value)) }}: {{ product.stock }}
+                                {{ capitalize($t('main.product.availability.' + product.availability.value)) }}: {{
+                                    product.stock }}
                             </span>
                         </div>
+                    </div>
+                    <div class="flex flex-row items-center">
+                        <span>Labels: {{ product.labels }}</span>
                     </div>
                 </UContainer>
                 <ProductOrganismsProductAddToCartForm :product="product" :cart-number="cartNumber"
                     v-model="productNumber" @change="updateProductsNumber" />
             </UContainer>
         </UContainer>
-        <ProductOrganismsProductCollapsibleMarkdown :title="capitalize($t('main.product.collapsible.description'))" :description="product.description" />
-        <ProductOrganismsProductCollapsibleMarkdown :title="capitalize($t('main.product.collapsible.ingredient'))" :description="product.ingredients" />
+        <UContainer class="flex flex-col">
+            <ProductMoleculesProductCollapsibleContent :title="capitalize($t('main.product.collapsible.description'))"
+                :description="product.description" />
+            <ProductMoleculesProductCollapsibleContent :title="capitalize($t('main.product.collapsible.ingredient'))"
+                :ingredients="product.ingredients" :allergies="product.allergies" :nutrition="product.nutrition" />
+            <ProductMoleculesProductCollapsibleContent
+                :title="capitalize($t('main.product.collapsible.technical-informations'))" :brand="product.marque"
+                :origin="product.origine" :tags="product.tags" />
+        </UContainer>
     </article>
 </template>
