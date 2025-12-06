c1bb0886e43c290acb006fc057da9e51640f0831
AurelDev <38510448+AeRogue666@users.noreply.github.com>
2025-12-06 17:44:25 +0100

    Updating productsFilterCtg with submitting categories system, Updating URLStore pinia store with new Nuxt routing element (navigateTo) replacing push and new submitting parameter/value system to work on queries

--- components/products/organisms/productsFilterCtg.vue
+++ components/products/organisms/productsFilterCtg.vue
@@ -1,71 +1,73 @@
 <script setup lang="ts">
-    import { UButton } from '#components';
-    import ProductsSelectMenu from '../molecules/productsMoleculesSelectMenu.vue';
+import { UButton } from '#components';
+import ProductsSelectMenu from '../molecules/productsMoleculesSelectMenu.vue';
 
-    const props = defineProps<{
-        categoriesList: Array<{
+const props = defineProps<{
+    categoriesList: Array<{
+        id: number,
+        label: string,
+        children: Array<{
             id: number,
             label: string,
-            children: Array<{
-                id: number,
-                label: string,
-                value: string,
-                checked?: boolean,
-            }>
+            value: string,
+            checked?: boolean,
         }>
-    }>();
+    }>
+}>();
 
-    const { updateQueryState, resetQuery } = URLStore();
-    const { currentRoute, push } = useRouter();
-    const filteredCategories = reactive<Array<string>>([]),
-        newFilteredCtg = ref<string | string[]>([]),
-        productSelectValue = ref<string>('pertinence');
+const { updateQueryState, resetQuery } = URLStore();
+const { currentRoute, push } = useRouter();
+const filteredCategories = reactive<Array<string>>([]),
+    newFilteredCtg = ref<string | string[]>([]),
+    productSelectValue = ref<string>('pertinence');
 
-    const updateSelectValue = (v: string) => {
-        productSelectValue.value = v
+const updateSelectValue = (v: string) => {
+    productSelectValue.value = v
+}
+
+const submitCategories = (value: string) => {
+    if (newFilteredCtg.value.length == 0) {
+        newFilteredCtg.value = value
+    } else {
+        if (newFilteredCtg.value.includes(value)) {
+            const index = newFilteredCtg.value.indexOf(value) + -1
+            if (index !== -1) {
+                newFilteredCtg.value = newFilteredCtg.value.slice(0, index)
+                console.log(newFilteredCtg.value, index)
+            } else {
+                newFilteredCtg.value = ''
+            }
+        } else {
+            newFilteredCtg.value = newFilteredCtg.value + ',' + value
+        }
     }
+    // newFilteredCtg.value.length == 0 ? newFilteredCtg.value = value : newFilteredCtg.value = newFilteredCtg.value + ',' + value
 
-    const submitCategories = (value: string) => {
-        newFilteredCtg.value.length == 0 ? newFilteredCtg.value = value : newFilteredCtg.value = newFilteredCtg.value + ',' + value
+    /* const index = filteredCategories.indexOf(value);
+    index > -1 ? filteredCategories.splice(index, 1) : filteredCategories.push(value) */
+};
 
-        /* const index = filteredCategories.indexOf(value);
-        index > -1 ? filteredCategories.splice(index, 1) : filteredCategories.push(value) */
+const resetCategoriesState = () => {
+    resetQuery();
+    newFilteredCtg.value = ''
+    console.log(filteredCategories, newFilteredCtg.value)
+},
+    checkCategoriesCheckedState = (array: { children: Array<{ id: number, label: string, value: string, checked?: boolean }> }[]) => {
+        array.map((obj: { children: any[]; }) => {
+            const findIndex = obj.children.findIndex((item: { checked: boolean; }) => item.checked == true);
+            if (findIndex > -1) {
+                return filteredCategories.push(obj.children[findIndex]?.value), useCategoriesStore().category = obj.children[findIndex]?.value
+            }
+        })
     };
 
-    const resetCategoriesState = () => {
-            resetQuery();
-            newFilteredCtg.value = ''
-            console.log(filteredCategories, newFilteredCtg.value)
-        },
-        checkCategoriesCheckedState = (array: { children: Array<{ id: number, label: string, value: string, checked?: boolean }> }[]) => {
-            array.map((obj: { children: any[]; }) => {
-                const findIndex = obj.children.findIndex((item: { checked: boolean; }) => item.checked == true);
-                if (findIndex > -1) {
-                    return filteredCategories.push(obj.children[findIndex]?.value), useCategoriesStore().category = obj.children[findIndex]?.value
-                }
-            })
-        };
-    
-    /* updateCategoriesState = (parameter: string, value: any) => {
-        if (!filteredCategories) {
-            resetQuery()
-        } else {
-            push({
-                query: {
-                    ...currentRoute.value.query,
-                    [parameter]: value,
-                }
-            })
-        }
-    }, */
+onMounted(() => {
+    checkCategoriesCheckedState(props.categoriesList)
+})
 
-    onMounted(() => {
-        checkCategoriesCheckedState(props.categoriesList)
-    })
-
-    watch(newFilteredCtg, (newValue) => {
-        console.log(newValue)
-    })
+watch(newFilteredCtg, (newValue) => {
+    console.log(newValue, newValue.length)
+})
 </script>
 
 <template>
@@ -79,9 +81,10 @@
                 @click.stop.prevent="resetCategoriesState" />
         </fieldset>
         <fieldset>
-            <ProductsSelectMenu :select-value="productSelectValue" @change="updateSelectValue" /> <!-- Sorting select -->
+            <ProductsSelectMenu :select-value="productSelectValue" @change="updateSelectValue" />
+            <!-- Sorting select -->
         </fieldset>
-        <ProductsMoleculesProductsMoleculeCollapsible :filtered-categories="filteredCategories" :categories-list="categoriesList"
-            @tag="submitCategories" /> <!-- Collapsible categories menu -->
+        <ProductsMoleculesProductsMoleculeCollapsible :filtered-categories="filteredCategories"
+            :categories-list="categoriesList" @tag="submitCategories" /> <!-- Collapsible categories menu -->
     </form>
 </template>
--- stores/URLStore.ts
+++ stores/URLStore.ts
@@ -1,18 +1,43 @@
 export const URLStore = defineStore("url", () => {
   const { query } = useRoute(),
     { currentRoute, push } = useRouter();
-  const updateQueryState = (parameter: string, value: string | string[]) => {
-    push({
-      query: {
-        ...currentRoute.value.query,
-        [parameter]: value,
-      },
-    });
+
+  const updateQueryState = async (
+    parameter: string,
+    value: string | string[]
+  ) => {
+    if (!value) {
+      if (currentRoute.value.fullPath.includes(parameter) && !value) {
+        const index = currentRoute.value.fullPath.indexOf(parameter);
+        if (index !== -1) {
+          currentRoute.value.fullPath = currentRoute.value.fullPath.slice(0, index);
+        }
+        await navigateTo({
+          path: currentRoute.value.fullPath
+        })
+      }
+    } else {
+      await navigateTo({
+        query: {
+          ...currentRoute.value.query,
+          [parameter]: value,
+        },
+      });
+      /* push({
+        query: {
+          ...currentRoute.value.query,
+          [parameter]: value,
+        },
+      }); */
+    }
   };
-  const resetQuery = () => {
-    push({
+  const resetQuery = async () => {
+    await navigateTo({
       query: {},
     });
+    /* push({
+      query: {},
+    }); */
   };
 
   return { updateQueryState, resetQuery };
